e446dbfeef09f2a56614f38d22ab465fe360781b
Victor Paranhos <paranhos_v@yahoo.com>
2016-03-21 23:02:15 -0300

    Adicao do bootstrap.

---
+++ resources/views/shared/nav.blade.php
@@ -0,0 +1,20 @@
+<nav class="navbar navbar-default">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                <span class="sr-only">Toggle Navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="/">Laravel - Test</a>
+        </div>
+
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav">
+                <li>{!! link_to_route('vendas.index', 'Ver Vendas') !!}</li>
+                <li>{!! link_to_route('vendas.create', 'Nova Venda') !!}</li>
+            </ul>
+        </div>
+    </div>
+</nav>
--- resources/views/vendas/index.blade.php
+++ resources/views/vendas/index.blade.php
@@ -1,24 +1,24 @@
 @extends($layout)
 
 @section('content')
-<h1>Vendas</h1>
-{!! link_to_route('vendas.create', 'Nova Venda') !!}
+  <h1>Vendas</h1>
+  {!! link_to_route('vendas.create', 'Nova Venda') !!}
 
-<table border="1">
-  <tr>
-    <th>Editar</th>
-    <th>Deletar</th>
-    <th>Título</th>
-  </tr>
-  @foreach ($vendas as $venda)
+  <table border="1">
     <tr>
-      <td> {!! link_to_route('vendas.edit', 'Editar', $venda->id) !!}</td>
-      <td>
-        {!! Form::open(['method' => 'DELETE', 'route' => ['vendas.destroy', $venda->id]]) !!}
-        <button type="submit">Deletar</button>
-        {!! Form::close() !!}
-      </td>
-      <td> {!! link_to_route('vendas.show', $venda->purchaser_name, $venda->id) !!}</td>
-  @endforeach
-</table>
+      <th>Editar</th>
+      <th>Deletar</th>
+      <th>Título</th>
+    </tr>
+    @foreach ($vendas as $venda)
+      <tr>
+        <td> {!! link_to_route('vendas.edit', 'Editar', $venda->id) !!}</td>
+        <td>
+          {!! Form::open(['method' => 'DELETE', 'route' => ['vendas.destroy', $venda->id]]) !!}
+          <button type="submit">Deletar</button>
+          {!! Form::close() !!}
+        </td>
+        <td> {!! link_to_route('vendas.show', $venda->purchaser_name, $venda->id) !!}</td>
+    @endforeach
+  </table>
 @endsection
